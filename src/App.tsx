01ee750cc31f4ccacad4fb60dd36e19fca116ecf
akhoz <adrian25092005@gmail.com>
2024-10-16 13:09:28 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-between w-screen h-screen">
+      <div className="flex items-center justify-center w-3/4">
+        Hello World
+      </div>
+      <div className="w-1/4 h-screen">
+        <img src="/images/hero.webp" className="h-screen" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
